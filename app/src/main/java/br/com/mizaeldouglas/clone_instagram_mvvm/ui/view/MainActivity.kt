0a8c4b901389f095eebda85d8ad4b5f6ba151b3b
MizaelDouglasDeMello <mizaeldouglas4@gmail.com>
2024-11-22 20:14:44 -0300

    feat aula sobre imagem e colunm e row

--- app/src/main/java/br/com/mizaeldouglas/clone_instagram_mvvm/ui/view/MainActivity.kt
+++ app/src/main/java/br/com/mizaeldouglas/clone_instagram_mvvm/ui/view/MainActivity.kt
@@ -4,21 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import br.com.mizaeldouglas.clone_instagram_mvvm.R
 import br.com.mizaeldouglas.clone_instagram_mvvm.ui.components.ButtonHome
 import br.com.mizaeldouglas.clone_instagram_mvvm.ui.theme.Clone_instagram_MVVMTheme
 
@@ -38,10 +47,12 @@ class MainActivity : ComponentActivity() {
 fun FirstApp() {
     Column(
         modifier = Modifier
-            .padding(16.dp)
             .fillMaxWidth()
             .fillMaxHeight()
-            .background(Color.DarkGray)
+//            .width(200.dp)
+//            .height(200.dp)
+            .border(20.dp, Color.Red)
+            .padding(30.dp, 10.dp)
 
     ) {
         ButtonHome(text = "Mizael Douglas")
@@ -50,6 +61,40 @@ fun FirstApp() {
                 .background(Color.Blue)
                 .fillMaxWidth()
         )
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Mizael Douglas", fontSize = 36.sp)
+            Column(
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(200.dp)
+                    .clip(CircleShape)
+                    .border(5.dp, Color.Red , CircleShape)
+                    .background(Color.Yellow),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+//                    .padding(30.dp, 30.dp, 30.dp, 30.dp)
+            ) { Text("MD", fontSize = 45.sp) }
+
+            Column(
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .border(5.dp, Color.Black , CircleShape)
+                    .background(Color.White),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.luffy),
+                    contentDescription = "luffytaro",
+                    modifier = Modifier
+                        .clip(CircleShape)
+                )
+            }
+        }
         Row(
             modifier = Modifier.fillMaxWidth().fillMaxHeight(),
             horizontalArrangement = Arrangement.SpaceBetween,
